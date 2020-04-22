35492211c25ae0c0904e1d3ef8da0ce09eb93590
Colby Allen <colbyallen012@gmail.com>
2020-04-22 09:15:06 -0500

    add question bank and setup home screen

--- Quizzler-iOS13/ViewController.swift
+++ Quizzler-iOS13/ViewController.swift
@@ -15,12 +15,21 @@ class ViewController: UIViewController {
     @IBOutlet weak var trueButton: UIButton!
     @IBOutlet weak var falseButton: UIButton!
     
+    let quiz = [
+        "Four + Two is equal to Six.",
+        "Five - Three is greater than One.",
+        "Three + Eight is less than Ten"
+    ]
+    
+    var questionNumber = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        questionLabel.text = quiz[questionNumber]
     }
 
     @IBAction func answerButtonPressed(_ sender: UIButton) {
+        
     }
     
 }
