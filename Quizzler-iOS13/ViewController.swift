578d3cf6eb6b4a2eef26043a8315c2e05765f781
Colby Allen <colbyallen012@gmail.com>
2020-04-22 09:49:27 -0500

    add check to see if the question answers are correct or not

--- Quizzler-iOS13/ViewController.swift
+++ Quizzler-iOS13/ViewController.swift
@@ -16,20 +16,35 @@ class ViewController: UIViewController {
     @IBOutlet weak var falseButton: UIButton!
     
     let quiz = [
-        "Four + Two is equal to Six.",
-        "Five - Three is greater than One.",
-        "Three + Eight is less than Ten"
+        ["Four + Two is equal to Six.", "True"],
+        ["Five - Three is greater than One.", "True"],
+        ["Three + Eight is less than Ten", "False"]
     ]
     
     var questionNumber = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        questionLabel.text = quiz[questionNumber]
+        updateUI()
     }
 
     @IBAction func answerButtonPressed(_ sender: UIButton) {
         
+        let userAnswer = sender.currentTitle
+        let actualAnswer = quiz[questionNumber][1]
+        
+        if userAnswer == actualAnswer {
+            print("Right")
+        } else {
+            print("Wrong")
+        }
+        
+        questionNumber += 1
+        updateUI()
+    }
+    
+    func updateUI() {
+        questionLabel.text = quiz[questionNumber][0]
     }
     
 }
