71473fa4cbcb24ad578be21e5fa1b9fc48a9db85
vivek <vivekp092@gmail.com>
2021-04-11 14:14:14 +0530

    added update ui method in view controller.

--- Quizzler-iOS13/ViewController.swift
+++ Quizzler-iOS13/ViewController.swift
@@ -10,11 +10,39 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    let quiz = ["Four","Five","Eight","Run"]
+    var selectedQuestion = -1
+    
+    @IBOutlet weak var questionText: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+    
     }
-
-
+    @IBAction func trueButtonAction(_ sender: Any) {
+        updateUI(isNext: true)
+    }
+    
+    @IBAction func falseButtonAction(_ sender: Any) {
+        
+        updateUI(isNext: false)
+        
+    }
+    
+    func updateUI(isNext : Bool) {
+        
+        if isNext && selectedQuestion < quiz.count {
+            selectedQuestion += 1
+            questionText.text = quiz[selectedQuestion]
+        } else {
+            if 0 < selectedQuestion && selectedQuestion < quiz.count {
+                selectedQuestion -= 1
+                questionText.text = quiz[selectedQuestion]
+            }
+        }
+    
+    }
 }
 
