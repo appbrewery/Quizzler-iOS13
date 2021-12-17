91f92bead77fce20cf3b3641fbc97024abf97ac0
leolenkhatho@gmail.com <leolenkhatho@gmail.com>
2021-12-17 16:40:21 +0200

    random question generation

--- Quizzler-iOS13/ViewController.swift
+++ Quizzler-iOS13/ViewController.swift
@@ -14,9 +14,15 @@ class ViewController: UIViewController {
     @IBOutlet weak var trueButton: UIButton!
     @IBOutlet weak var falseButton: UIButton!
     @IBOutlet weak var progressBar: UIProgressView!
+    
+    let quiz = [
+        "You are going to be Java 11 OCP certified by 8 July 2022",
+        "You are going to master IOS develpment by 8 July 2022",
+        "You are going to master JavaScript and NodeJS by 8 July 2022"
+    ]
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        questionLabel.text = quiz.randomElement()
     }
 
     @IBAction func answerButtonPressed(_ sender: UIButton) {
