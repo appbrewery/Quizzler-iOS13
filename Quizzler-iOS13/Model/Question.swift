fb6c5b6b7ff7991e9b7f1c86aeaf9845c64f830f
Jalaj <83359199+Jalaj2002@users.noreply.github.com>
2023-01-06 01:21:43 +0530

    Add files via upload

---
+++ Quizzler-iOS13/Model/Question.swift
@@ -0,0 +1,17 @@
+//
+//  Question.swift
+//  Quizzler-iOS13
+//
+//  Created by Jalaj's Macbook  on 27/09/22.
+//  Copyright © 2022 The App Brewery. All rights reserved.
+//
+
+import Foundation
+struct Question{
+    let text:String
+    let answer:String
+    init(q:String,a:String){
+        text=q
+        answer=a
+    }
+}
