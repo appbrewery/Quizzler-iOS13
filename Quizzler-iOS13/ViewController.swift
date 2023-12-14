e4547ed373d097922d20cb818885f7221ba4634c
Abdulkadir Akyurt <akyurt.a@gmx.de>
2023-12-14 10:52:03 +0100

    added if else to make buttons activating

--- Quizzler-iOS13/ViewController.swift
+++ Quizzler-iOS13/ViewController.swift
@@ -18,20 +18,37 @@ class ViewController: UIViewController {
     
     let quiz = [
         ["Four + Two is equal to Six", "True"],
-        ["Four - Two is equal toOne", "False"],
-        ["Four * Two is equal to Eight", "True"]
+        ["Four - Two is equal to Two", "True"],
+        ["Four * Two is equal to Five", "False"]
     ]
     
     var questionNumber = 0
     
+   
     override func viewDidLoad() {
         super.viewDidLoad()
         updateUI()
     }
     
     @IBAction func answerButtonPressed(_ sender: UIButton) {
-        questionNumber += 1
+        
+        let userAnswer = sender.currentTitle  // true or false
+        let actualAnswer = quiz[questionNumber][1]
+        
+        if userAnswer == actualAnswer {
+            print("Right")
+        } else {
+            print("False")
+        }
+        if questionNumber + 1 < quiz.count {
+            questionNumber += 1
+            
+        } else {
+            questionNumber = 0
+        }
+        
         updateUI()
+        
     }
     
     func updateUI() {
